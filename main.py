65fad97e473efc96d37a7ebcb1942445e4d55037
RA4Z <rraz639@gmail.com>
2023-07-07 11:07:55 -0300

    Fatiando a string

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(url)
+url = "bytebank.com/cambio?moedaOrigem=real"
+
+url_base = url[0:19]
+url_parametros = url[20:36]
+
+print(url_base)
+print(url_parametros)
