528f4397ca17507e82bc3e3732865bb017747c7e
RA4Z <rraz639@gmail.com>
2023-07-07 12:52:25 -0300

    Utilizando len para ver o tamanho da string

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
 indice_interrogacao = url.find('?')
 url_base = url[0:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
 
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
 print(url)
 print(url_base)
 print(url_parametros)
+print(valor)
