767475048416c235386c52bd685727b59de7d3b1
RA4Z <rraz639@gmail.com>
2023-07-07 14:54:29 -0300

    URL com múltiplos parâmetros

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+# SEPARA BASE E OS PARÂMETROS
 indice_interrogacao = url.find('?')
 url_base = url[0:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
 
-parametro_busca = 'moedaOrigem'
+# BUSCA O VALOR DE UM PARÂMETRO
+parametro_busca = 'moedaDestino'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
 
-print(url)
-print(url_base)
-print(url_parametros)
-print(valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+# ESCREVE OS DADOS NO TERMINAL
+print('\n-------------\n')
+print(valor.title())
+print('\n-------------\n')
